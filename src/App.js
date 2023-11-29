6724b5cb06a7cb34a5019382167f288ea59ba324
RajuBudhoju <budhojuraju>
2023-11-29 09:13:34 +0530

    User Add movie feature added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import './App.css';
 function App() {
   const [movies,setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isAddingMovie, setIsAddingMovie] = useState(false); // New state for showing/hiding the form
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    openingText: '',
+    releaseDate: ''
+  });
 
   async function fetchMoviesHandler() {
     try {
@@ -27,15 +33,57 @@ function App() {
    
   }
 
+  const addMovieHandler = () => {
+    setMovies((preMovies) => [
+      ...preMovies, 
+      {
+         id: Math.random().toString(), ...newMovie
+      }
+    ]);
+    setIsAddingMovie(false);
+    setNewMovie({
+      title: '',
+      openingText: '',
+      releaseDate: ''
+    });
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={() => setIsAddingMovie(true)}>Add Movie</button>
       </section>
       <section>
+        {isAddingMovie && (
+            <form>
+              <label>Title:</label>
+              <input
+                type="text"
+                value={newMovie.title}
+                onChange={(e) => setNewMovie((prev) => ({ ...prev, title: e.target.value }))}
+              />
+              <label>Opening Text:</label>
+              <textarea
+                rows="4"
+                value={newMovie.openingText}
+                onChange={(e) => setNewMovie((prev) => ({ ...prev, openingText: e.target.value }))}
+              />
+              <label>Release Date:</label>
+              <input
+                type="text"
+                value={newMovie.releaseDate}
+                onChange={(e) => setNewMovie((prev) => ({ ...prev, releaseDate: e.target.value }))}
+              />
+              <button type="button" onClick={addMovieHandler}>
+                Add Movie
+              </button>
+            </form>
+        )}
         {isLoading && <p>Loading...</p>}
         <MoviesList movies={movies} />
       </section>
+
     </React.Fragment>
   );
 }
